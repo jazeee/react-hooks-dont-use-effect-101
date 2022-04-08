23428e7b0748a98f7622298b149dd0275b06639f
Jaz Singh <jaz@color.com>
2022-04-08 13:41:24 -0700

    Convert to an array of components, add a button.

--- src/UseEffect/AntiPatterns/AntiPatterns.tsx
+++ src/UseEffect/AntiPatterns/AntiPatterns.tsx
@@ -1,7 +1,9 @@
+import { Fragment, useState } from 'react';
 import { ApiRequestAntiPattern } from './Examples/ApiRequest';
 import { CopyPropsAntiPattern } from './Examples/CopyProps';
 import { DerivedPropsAntiPattern } from './Examples/DerivedProps';
 import { ExternalReferenceAntiPattern } from './Examples/ExternalReference';
+import { ImperativeChangeAntiPattern } from './Examples/ImperativeChange';
 import { InfiniteLoopAntiPattern } from './Examples/InfinteLoop';
 import { LocalOrRemoteAntiPattern } from './Examples/LocalOrRemoteProp';
 import { ObjectUpdateInfiniteLoopAntiPattern } from './Examples/ObjectUpdateInfinteLoop ';
@@ -10,17 +12,68 @@ interface Props {
   firstName: string;
 }
 
+const EXAMPLES: {
+  name: string;
+  Component: React.FC<Props>;
+}[] = [
+  {
+    name: 'Copying Props',
+    Component: CopyPropsAntiPattern,
+  },
+  {
+    name: 'Derived/Calculated values from Props',
+    Component: DerivedPropsAntiPattern,
+  },
+  {
+    name: 'External Reference in a component',
+    Component: ExternalReferenceAntiPattern,
+  },
+  {
+    name: 'Recursive infinite loop',
+    Component: InfiniteLoopAntiPattern,
+  },
+  {
+    name: 'Object reference infinite loop',
+    Component: ObjectUpdateInfiniteLoopAntiPattern,
+  },
+  {
+    name: 'Mix and match props',
+    Component: LocalOrRemoteAntiPattern,
+  },
+  {
+    name: 'Duplicating existing state management',
+    Component: ApiRequestAntiPattern,
+  },
+  {
+    name: 'Imperative magic',
+    Component: ImperativeChangeAntiPattern,
+  },
+];
+
+const EXAMPLE_NAMES = EXAMPLES.map((example) => example.name);
+
 export function UseEffectAntiPatterns(props: Props) {
+  const [visibleComponentName, setVisibleComponentName] = useState(EXAMPLE_NAMES[0]);
   const { firstName } = props;
+  const VisibleComponent = EXAMPLES.find(
+    (example) => example.name === visibleComponentName
+  )?.Component;
   return (
     <>
-      <CopyPropsAntiPattern firstName={firstName} />
-      <DerivedPropsAntiPattern firstName={firstName} />
-      <ExternalReferenceAntiPattern firstName={firstName} />
-      <InfiniteLoopAntiPattern firstName={firstName} />
-      <ObjectUpdateInfiniteLoopAntiPattern firstName={firstName} />
-      <LocalOrRemoteAntiPattern firstName={firstName} />
-      <ApiRequestAntiPattern firstName={firstName} />
+      {EXAMPLE_NAMES.map((name) => {
+        const exampleIsSelected = name === visibleComponentName;
+        return (
+          <button
+            key={name}
+            type="button"
+            onClick={() => setVisibleComponentName(name)}
+            style={{ backgroundColor: exampleIsSelected ? 'lightgreen' : undefined, margin: 4 }}
+          >
+            {name}
+          </button>
+        );
+      })}
+      {VisibleComponent && <VisibleComponent firstName={firstName} />}
     </>
   );
 }
